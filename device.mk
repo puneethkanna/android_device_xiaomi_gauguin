c40038e341ba23250672e74b146ea7c16b9dc49e
baolong24 <1667375308@qq.com>
2021-03-16 17:02:10 +0800

    Revert "gauguin: Update keylayout"
    This reverts commit e285709e073c4030edd327c2bec13076016cbe88.

--- device.mk
+++ device.mk
@@ -104,8 +104,7 @@ PRODUCT_BOOT_JARS += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl \
-    $(LOCAL_PATH)/keylayout/lito-lagoonqrd-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/lito-lagoonqrd-snd-card-mtp_Button_Jack.kl
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
 # Lights
 PRODUCT_PACKAGES += \
