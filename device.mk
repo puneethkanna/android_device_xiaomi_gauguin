deb3cb4bc525ae9dd459774d41cb232346fa80df
baolong24 <1667375308@qq.com>
2021-04-03 23:08:07 +0800

    gauguin: Drop Power HAL

--- device.mk
+++ device.mk
@@ -135,13 +135,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.wifi.rtt.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.gauguin \
-    android.hardware.power.stats@1.0-service
-
 # Powerstats
 PRODUCT_PACKAGES += \
+    android.hardware.power.stats@1.0-service \
     android.hardware.power.stats@1.0-service.mock
 
 # Properties
