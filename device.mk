4581e251f4eee868167f98176bd89589fac28ead
baolong24 <1667375308@qq.com>
2021-04-04 09:22:23 +0800

    Revert "gauguin: Import Powerstats"
    This reverts commit b082ce2a44b8bd74c0331aa3b4aea75f32e67352.

--- device.mk
+++ device.mk
@@ -135,11 +135,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.wifi.rtt.xml
 
-# Powerstats
-PRODUCT_PACKAGES += \
-    android.hardware.power.stats@1.0-service \
-    android.hardware.power.stats@1.0-service.mock
-
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
 -include $(LOCAL_PATH)/product_prop.mk
