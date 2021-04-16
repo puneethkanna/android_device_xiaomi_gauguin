dc3af6a55a7631460dc46f2ad1acc20d2b72574e
puneethkanna <neerukondapuneeth@gmail.com>
2021-04-16 08:12:45 +0000

    p404 tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_gauguin.mk
+    $(LOCAL_DIR)/p404_gauguin.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_gauguin-user \
-    aosp_gauguin-userdebug \
-    aosp_gauguin-eng
+    p404_gauguin-user \
+    p404_gauguin-userdebug \
+    p404_gauguin-eng
