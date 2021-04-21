b5a8d2933c93b082639722303a09da4d5790902f
puneethkanna <neerukondapuneeth@gmail.com>
2021-04-21 16:26:48 +0530

    cloned from havoc

--- lineage_gauguin.mk
+++ lineage_gauguin.mk
@@ -7,14 +7,12 @@
 # Inherit from gauguin device
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-CUSTOM_BUILD_TYPE := baolong24
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_gauguin
