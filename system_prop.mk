dd2a2ac4b7ec5ec90ea5cfb3d976e05d41506d7e
puneethkanna <neerukondapuneeth@gmail.com>
2021-05-15 12:19:48 +0000

    Enable content basedSF refresh rate settings

--- system_prop.mk
+++ system_prop.mk
@@ -37,6 +37,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.netflix.bsp_rev=Q7250-19133-1
 
+# Refresh Rate
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=200 \
+    ro.surface_flinger.set_touch_timer_ms=200
+
 # RIL
 PRODUCT_PRODUCTY_OVERRIDES += \
     ro.telephony.default_network=33,22
